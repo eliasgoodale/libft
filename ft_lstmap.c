16d160ba9f99b8d2163c0a2351cba9c803e9cca7
Elias Goodale <egoodale@e1z3r9p24.42.us.org>
2018-02-27 18:23:05 -0800

    ez

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,33 +6,12 @@
 /*   By: egoodale <eligoodale1@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 14:33:38 by egoodale          #+#    #+#             */
-/*   Updated: 2018/02/27 11:39:25 by egoodale         ###   ########.fr       */
+/*   Updated: 2018/02/27 18:20:23 by egoodale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
-{
-	t_list	*new;
-	t_list	*tmp;
-	t_list	*begin;
-
-	tmp = f(lst);
-	if (!(new = ft_lstnew(tmp->content, tmp->content_size)))
-		return (NULL);
-	begin = new;
-	lst = lst->next;
-	while (lst)
-	{
-		tmp = f(lst);
-		if (!(new->next = ft_lstnew(tmp->content, tmp->content_size)))
-			return (NULL);
-		new = new->next;
-		lst = lst->next;
-	}
-	return (begin);
-}*/
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list		*result;
@@ -48,11 +27,11 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	head = result;
 	while (lst)
 	{
-	  elem = f(lst);
-	  if (!(result->next = ft_lstnew(elem->content, elem->content_size)))
-		  return (NULL);
-	  result = result->next;
-	  lst = lst->next;
-  }
+		elem = f(lst);
+		if (!(result->next = ft_lstnew(elem->content, elem->content_size)))
+			return (NULL);
+		result = result->next;
+		lst = lst->next;
+	}
 	return (head);
 }
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,64 +6,35 @@
 /*   By: egoodale <eligoodale1@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 12:58:07 by egoodale          #+#    #+#             */
-/*   Updated: 2018/02/27 11:23:02 by egoodale         ###   ########.fr       */
+/*   Updated: 2018/02/27 18:17:53 by egoodale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*t_list	*ft_lstnew(void const *content, size_t c_s)
-{
-	t_list	*new_list;
-
-	if (!(new_list = (t_list *)malloc(sizeof(*new_list))))
-		return (NULL);
-	if (new_list)
-	{
-		if (content)
-		{
-			new_list->content = malloc(sizeof(c_s));
-			if (!new_list->content)
-			{
-				free(new_list);
-				return (NULL);
-			}
-			new_list->content = ft_memcpy(new_list->content, content, c_s);
-			new_list->content_size = c_s;
-		}
-		else
-		{
-			new_list->content = NULL;
-			new_list->content_size = 0;
-		}
-		new_list->next = NULL;
-	}
-	return (new_list);
-}*/
-
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list *retval;
+	t_list *ret;
 
-	retval = (t_list*)malloc(sizeof(t_list));
-	if (retval == NULL)
+	ret = (t_list*)malloc(sizeof(t_list));
+	if (ret == NULL)
 		return (NULL);
 	if (content == NULL)
 	{
-		retval->content = NULL;
-		retval->content_size = 0;
+		ret->content = NULL;
+		ret->content_size = 0;
 	}
 	else
 	{
-		retval->content = malloc(content_size);
+		ret->content = malloc(content_size);
 		if (content == NULL)
 		{
-			free(retval);
+			free(ret);
 			return (NULL);
 		}
-		ft_memcpy(retval->content, content, content_size);
-		retval->content_size = content_size;
+		ft_memcpy(ret->content, content, content_size);
+		ret->content_size = content_size;
 	}
-	retval->next = NULL;
-	return (retval);
+	ret->next = NULL;
+	return (ret);
 }
